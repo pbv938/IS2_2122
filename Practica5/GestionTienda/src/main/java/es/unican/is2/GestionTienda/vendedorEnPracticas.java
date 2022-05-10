3c8de3f1eab11a22f7eaca1c32bff5c6e444736e
Pedro Berrio Valdor <pbv938@alumnos.unican.es>
2022-05-10 14:18:34 +0200

    Practica5

---
+++ Practica5/GestionTienda/src/main/java/es/unican/is2/GestionTienda/vendedorEnPracticas.java
@@ -0,0 +1,29 @@
+package es.unican.is2.GestionTienda;
+
+
+public class vendedorEnPracticas extends Vendedor {
+	
+	private String dni;
+	
+	/**
+	 * Retorna un nuevo vendedor en prácticas
+	 * @param nombre
+	 * @param dni
+	 */
+	public vendedorEnPracticas(String nombre, String id, String dni) {//WMC+1
+		super(nombre, id);
+		this.dni= dni;
+	}
+	
+	public String getDni() {//WMC+1
+		return dni;
+	}
+
+	@Override
+	public boolean equals(Object obj) {//WMC+1
+		if (!(obj instanceof vendedorEnPracticas)) //WMC+1 //CCog+1
+			return false;
+		vendedorEnPracticas v = (vendedorEnPracticas) obj;
+		return (v.getId().equals(getId()) && v.getDni().equals(getDni()));//CCog+1
+	}
+}
